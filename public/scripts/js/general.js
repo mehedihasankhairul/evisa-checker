775a249a6e9819730c9d5cd7838e7a44e5e7b60d
Mehedi Hasan Khairul <mehedihasankhairul@Mehedis-MacBook-Air.local>
2025-01-15 05:35:42 +0600

    image path change

--- public/scripts/js/general.js
+++ public/scripts/js/general.js
@@ -25,7 +25,6 @@ const users = [
 ];
 
 let currentUser = users; // Preselect the first user for simplicity
-let mainLogo = "/public/images/img/logo.png";
 
 // Generate unique URL for a user
 function generateUserUrl(referenceNumber) {
@@ -170,7 +169,7 @@ function renderUserInfo(userDetails) {
         <table width="800" cellpadding="2" cellspacing="2" style="border-bottom:2px solid black;">
           <tr>
               <td align="left" valign="top">
-                  <img src=${mainLogo} width="90" />
+                  <img src=`/ public / images / img / mainLogo.png` width="90" />
               </td>
               <td align="left" valign="top" style="line-height:22px;font-size:17px;">
                   <b>
