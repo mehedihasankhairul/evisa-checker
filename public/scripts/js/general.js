609b7c17a4e6018852418a3f3bac0e6c5c718c2f
Mehedi Hasan Khairul <mhkhairul78@gmail.com>
2025-04-29 22:34:59 +0600

    Update general.js

--- public/scripts/js/general.js
+++ public/scripts/js/general.js
@@ -239,10 +239,10 @@ const userInfoHtml = `
                         <td>Жол жүрүүчү документтин (паспорттун) бүткөн датасы/<br> Date of expiry of the travelling document (passport):</td>
                         <td>${userDetails.expiryDate}</td>
                       </tr>
-                      <tr>
-                        <td>Бирдиктүү документтин мөөнөтү / <br> Validity of uniform permit:</td>
-                        <td>${userDetails.uniformPermit}</td>
-                      </tr>
+                      // <tr>
+                      //   <td>Бирдиктүү документтин мөөнөтү / <br> Validity of uniform permit:</td>
+                      //   <td>${userDetails.uniformPermit}</td>
+                      // </tr>
                       <tr>
                         <td>Визанын түрү/Type of visa:</td>
                         <td>${userDetails.visaType}</td>
@@ -267,10 +267,10 @@ const userInfoHtml = `
                         <td>Чакыруу тараптын жеке салык номери/ <br> Inviting party's individual taxpayer number:</td>
                         <td>${userDetails.invitingPartyTIN}</td>
                       </tr>
-                       <tr>
-                        <td>Иштөөгө уруксут/The right to work:</td>
-                        <td>${userDetails.rightToWork}</td>
-                      </tr>
+                      //  <tr>
+                      //   <td>Иштөөгө уруксут/The right to work:</td>
+                      //   <td>${userDetails.rightToWork}</td>
+                      // </tr>
                       <tr>
                         <td>Берилген датасы/Date of issue:</td>
                         <td>${userDetails.dateOfIssue}</td>
